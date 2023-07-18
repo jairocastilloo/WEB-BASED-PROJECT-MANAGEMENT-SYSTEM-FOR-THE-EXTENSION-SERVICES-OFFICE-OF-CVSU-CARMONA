6a91c68993f3cc2e31ed23d7adf32a5732fc732c
jairocastilloo <vajairocastillO@gmail.com>
2023-07-18 12:36:08 +0800

    submitoutput

--- resources/views/activity/submitoutput.blade.php
+++ resources/views/activity/submitoutput.blade.php
@@ -11,28 +11,51 @@
             <h6><b>Activity: {{ $activity['actname'] }}</b></h6>
         </div>
         <div class="col-4 p-2 pt-3 border-end text-center position-triangle">
-            <h6><b>Output</b></h6>
+            <h6><b>Output <span class="small">>></span> {{ $outputtype }}</b></h6>
         </div>
     </div>
 
     <div class="container">
         <div class="row">
-            <div class="col-8">
+            <div class="col-1">
 
+
+            </div>
+            <div class="col-10">
                 <div class="basiccont p-2">
-                    <div class="border-bottom ps-1">
-                        <h6 class="fw-bold small">Output to be Submitted</h6>
+                    <div class="border-bottom ps-1 mb-2">
+                        <h6 class="small"><b>Output to be Submitted - </b>{{ $outputtype }}</h6>
                     </div>
+                    @foreach ($currentoutputtype as $currentoutput)
+                    <div class="mb-3">
+                        <label class="form-label">{{ $currentoutput['output_name'] }}:</label>
+                        <input type="number" class="form-control" id="hours-rendered-input" placeholder="Enter the quantity" min="0" step="1">
+
+                    </div>
+                    @endforeach
+
+
 
                 </div>
-            </div>
-            <div class="col-4">
                 <div class="basiccont p-2">
-                    <div class="border-bottom ps-1">
+                    <div class="border-bottom ps-1 mb-2">
+                        <h6 class="fw-bold small">Facilitator Involved</h6>
+                    </div>
+                    <button type="button" class="btn btn-outline-secondary btn-sm mt-2" id="submitreport-btn">Add Facilitator</button>
+                </div>
+                <div class="basiccont p-2">
+                    <div class="border-bottom ps-1 mb-2">
                         <h6 class="fw-bold small">Supporting documents</h6>
                     </div>
-
+                    <label class="form-label" for="customFile">Submit Activity Report:</label>
+                    <input type="file" class="form-control" id="customFile" accept=".docx" />
+                    <div class="d-flex justify-content-center">
+                        <button type="button" class="btn btn-outline-primary mt-2" id="submitreport-btn">Submit Report</button>
+                    </div>
                 </div>
+            </div>
+            <div class="col-1">
+
             </div>
 
         </div>
