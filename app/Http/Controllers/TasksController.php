a415f4cffa78f21a6401c07ad529550066ba5178
jairocastilloo <vajairocastillO@gmail.com>
2023-08-29 22:53:42 +0800

    Select calendar year response

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -102,13 +102,9 @@ class TasksController extends Controller
             $inCurrentYear = false;
         }
 
-
         $currentproject = $user->projects()
             ->where('calendaryear', $currentYear)
             ->get();
-
-
-
 
         $projectIds = $currentproject->pluck('id')->toArray();
 
@@ -119,10 +115,28 @@ class TasksController extends Controller
 
         $activityIds = $activities->pluck('id')->toArray();
 
-        $subtasks = $user->subtasks()
+        $dueInOneWeek = clone $currentDate;
+        $dueInOneWeek->modify('+7 days');
+
+        $dueInThirtyDays = clone $dueInOneWeek;
+        $dueInThirtyDays->modify('+30 days');
+
+        $taskDueThisSevenDays = $user->subtasks()
+            ->where('subduedate', '>=', $currentDate)
+            ->where('subduedate', '<=', $dueInOneWeek)
             ->whereIn('activity_id', $activityIds)
             ->get();
 
+        $taskDueWithinNextThirtyDays = $user->subtasks()
+            ->where('subduedate', '>', $dueInOneWeek)
+            ->where('subduedate', '<=', $dueInThirtyDays)
+            ->whereIn('activity_id', $activityIds)
+            ->get();
+
+        $taskDueLater = $user->subtasks()
+            ->where('subduedate', '>', $dueInThirtyDays)
+            ->whereIn('activity_id', $activityIds)
+            ->get();
 
         $contributions = SubtaskContributor::where('user_id', $userid)
             ->where('approval', 1)
@@ -133,11 +147,13 @@ class TasksController extends Controller
         return view('implementer.index', [
             'currentproject' => $currentproject,
             'activities' => $activities,
-            'subtasks' => $subtasks,
             'contributions' => $contributions,
             'calendaryears' => $calendaryears,
             'inCurrentYear' => $inCurrentYear,
-            'currentYear' => $currentYear
+            'currentYear' => $currentYear,
+            'taskDueThisSevenDays' => $taskDueThisSevenDays,
+            'taskDueWithinNextThirtyDays' => $taskDueWithinNextThirtyDays,
+            'taskDueLater' => $taskDueLater,
         ]);
     }
 
