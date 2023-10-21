4d8131ff59954b82f022f2e47260f757d14ffea7
jce12 <jhayestanero@gmail.com>
2023-10-21 18:14:51 +0800

    fix-slider-with-image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,13 +2,39 @@
 
 @section('content')
 <section class="maincontainer homeloginpage full-page-container py-5">
-    <div class="container-fluid">
+    
+    <div class="container p-0">
         <div class="row g-0 border rowjonel">
-            <div class="col-lg-6 text-center d-flex align-items-center justify-content-center">
-                <div class="d-flex flex-column gap-4 align-items-center justify-content-center">
-                    <div class="logos"><img  src="{{ asset('images/Picture1.png')}}" alt="Extension Service Office" width="350px"></div>
-                    <div><h6 class="fw-bold">Cavite States University - Carmona</h6><h2 class="fw-bold text-uppercase">Extension Service Office</h2></div>
-                </div> 
+            <div class="col-lg-6 text-center d-flex align-items-center justify-content-center p-0">
+            <div id="myCarousel" class="carousel slide d-flex w-100 h-100 rowjonel2" data-ride="carousel">
+                    
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <div class="carousel-inner d-flex w-100 h-100 rowjonel2">
+                        <div class="carousel-item active">
+                            <img class="w-100 h-100" src="{{ asset('images/Slider4.png')}}" alt="Slide 1">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="{{ asset('images/Slider2.png')}}" alt="Slide 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="w-100 h-100" src="{{ asset('images/Slider3.png')}}" alt="Slide 3">
+                        </div>
+                    </div>
+
+                        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                </div>
             </div>
             
             <div class="col-lg-6">
