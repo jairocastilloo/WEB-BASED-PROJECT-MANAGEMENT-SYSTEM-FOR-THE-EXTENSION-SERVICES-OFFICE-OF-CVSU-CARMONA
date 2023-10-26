e7fd0265a8c614810b5ca446afcfa6d35c02d53e
jaisenpa1 <vajairocastillo@gmail.com>
2023-10-27 06:22:15 +0800

    as

---
+++ app/Http/Livewire/TerminalReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TerminalReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.terminal-report');
+    }
+}
